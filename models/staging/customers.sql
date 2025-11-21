6fc0f38082d7bcb2af41519d5f35c40eff7accac
sudheer reddy <sudheerreddy.k@techstargroup.com>
2025-11-21 09:15:46 +0000

    commit

--- models/staging/customers.sql
+++ models/staging/customers.sql
@@ -5,7 +5,7 @@ with tb2 as (
         id,
         first_name,
         last_name 
-    from {{ source('datafeed_shared_schema', 'STG_CUSTOMERS') }}
+    from {{ source('datafeed_shared_schema', 'STG_CUSTOMER_DATA') }}
 )
 
 select * from tb2
